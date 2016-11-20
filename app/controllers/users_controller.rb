2d1a02f29324e066c66c1d40bb704598b70cfd99
solvar_kvedalen <solvar.kvedalen@gmail.com>
2016-11-20 12:27:36 +0000

    added restrictions in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
     
   before_action :set_user, only: [:edit, :update, :show]
-  
+  before_action :require_same_user, only: [:edit, :update]
     
-   def new
-     @user = User.new
-   end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Alpha blog"
+      flash[:success] = "Welcome to the Alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -50,6 +50,14 @@ class UsersController < ApplicationController
         :email,
         :password)
   end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
   
 end
   
